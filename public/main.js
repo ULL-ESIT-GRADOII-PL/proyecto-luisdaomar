331b48cdda2ba46a84b7a6b35a2bf40a146374fb
alu0100819182 <alu0100819182@ull.edu.es>
2016-07-06 20:32:17 +0100

    Cambios para ver arbol

--- public/main.js
+++ public/main.js
@@ -3,23 +3,21 @@ $(document).ready(function() {
     try {
       var result = pl0.parse($('#input').val());
       $('#output').html(JSON.stringify(result,undefined,2));
+      semantic(result);
     } catch (e) {
       $('#output').html('<div class="error"><pre>\n' + JSON.stringify(e, null,4) + '\n</pre></div>');
     }
   });
 
   $("#examples").change(function(ev) {
-    var f = ev.target.files[0]; 
+    var f = ev.target.files[0];
     var r = new FileReader();
-    r.onload = function(e) { 
+    r.onload = function(e) {
       var contents = e.target.result;
-      
+
       input.innerHTML = contents;
     }
     r.readAsText(f);
   });
 
 });
-
-  
-
